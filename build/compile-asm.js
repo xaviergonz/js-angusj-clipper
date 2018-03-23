8d1e1dba33bf9c711a9e2e98f6b51c6152a3fa40
Javier Gonzalez <xaviergonz@gmail.com>
2018-03-23 19:57:31 +0100

    now using docker to compile on any OS

---
+++ build/compile-asm.js
@@ -0,0 +1,28 @@
+const shelljs = require('shelljs');
+const path = require('path');
+
+const wasmDir = path.join(__dirname, '..', 'src', 'wasm');
+console.log(`using "${wasmDir}" as wasm dir`);
+
+function build(wasmMode) {
+    const extraOptions = wasmMode ? `-s WASM=1 -O2` : `--memory-init-file 0 -O3`;
+    const output = wasmMode ? `clipper.js` : `clipper-wasm.js`;
+
+    const cmd = `docker run --rm -v ${wasmDir}:/src trzeci/emscripten emcc --pre-js prefix.js --post-js postfix.js --bind -s ALLOW_MEMORY_GROWTH=1 -s NO_EXIT_RUNTIME=1 ${extraOptions} clipper.cpp -o ${output}`;
+    const returnData = shelljs.exec(cmd);
+    if (returnData.code !== 0) {
+        console.error(`build failed with error code ${returnData.code}`);
+        process.exit(returnData.code);
+    }
+
+    shelljs.mkdir('dist', 'dist/wasm');
+    shelljs.cp(`src/wasm/${output}`, `dist/wasm/${output}`);
+    if (wasmMode) {
+        shelljs.cp(`src/wasm/clipper-wasm.wasm`, `dist/wasm/clipper-wasm.wasm`);        
+    }
+}
+
+console.log('building asmjs version');
+build(false);
+console.log('building wasm version');
+build(true);
